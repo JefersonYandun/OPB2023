dfbe0ba08f144d7681f355a11a5319de22c67837
JefersonYandun <jeferson.yandun237@ist17dejulio.edu.ec>
2023-11-20 06:07:38 -0500

    Pantalla Retsa

---
+++ app/src/main/java/com/jsyp/operacionesbas/Resta.java
@@ -0,0 +1,14 @@
+package com.jsyp.operacionesbas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resta);
+    }
+}
